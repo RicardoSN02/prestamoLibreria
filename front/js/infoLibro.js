bfb6e48cf3718dd0cf483ac349ef09f882e6847d
NallelyRAM <nallely.ramirez216724@potros.itson.edu.mx>
2023-11-27 16:38:22 -0700

    formato fecha infoLibro.js
    Falta que muestre la imagen y estado

--- front/js/infoLibro.js
+++ front/js/infoLibro.js
@@ -3,11 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
     var libroString = urlParams.get('libro');
     var libro = JSON.parse(decodeURIComponent(libroString));
 
-    document.getElementById('imagenLibro').src = 'data:image/png;base64,' + libro.imagenLibro;
+    console.log("info Imagen:", libro.imagenLibro);
+
+    if (libro.imagenLibro) {
+        var imagenResultado = document.getElementById('imagenLibro');
+        imagenResultado.src = 'data:image/png;base64,' + libro.imagenLibro;
+        imagenResultado.alt = libro.titulo;
+    } else {
+        console.error('Error: libro.imagenLibro está indefinido o nulo.');
+    }
+
     document.getElementById('titulo').innerText = libro.titulo;
     document.getElementById('autor').innerText = libro.autor;
     document.getElementById('editorial').innerText = libro.editorial;
-    document.getElementById('fechaPublicacion').innerText = libro.fechaPublicacion;
+
+    var fechaPublicacion = moment(libro.fechaPublicacion);
+    var formatoFecha = fechaPublicacion.format('DD/MM/YYYY');
+
+    document.getElementById('fechaPublicacion').innerText = formatoFecha;
+
     document.getElementById('categoria').innerText = libro.categoria;
-    document.getElementById('resumen').innerText = libro.resumen; 
+    document.getElementById('resumen').innerText = libro.resumen;
 });
